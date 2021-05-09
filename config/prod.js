13bf00fdc09ce67fd9eda8379a06da4cb7a0891b
Eryk Brol <brol.eryk@gmail.com>
2021-05-08 20:20:10 -0400

    Rename keys to use camelCase

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
    mongoURI: process.env.MONGO_URI,
    cookieKey: process.env.COOKIE_KEY,
-   s3_access_key: process.env.S3_ACCESS_KEY,
-   s3_access_secret: process.env.S3_ACCESS_SECRET,
+   s3AccessKey: process.env.S3_ACCESS_KEY,
+   s3AccessSecret: process.env.S3_ACCESS_SECRET,
    port: process.env.PORT,
 };
